ed36da264b74e70df596f94ec0de87edd27a5dcd
eddsFrik <129126536+eddsFrik@users.noreply.github.com>
2025-01-31 22:31:22 +0000

    working-on-playRound-function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let humanScore = 0;
+let computerScore = 0;
+
+
 function getComputerChoice() {
 
     let randomValue = Math.floor(Math.random() * 3) + 1;
@@ -17,7 +21,6 @@ function getComputerChoice() {
 }
 
 
-
 function getHumanChoice() {
 
     let choice = prompt('Make your choice...');
@@ -26,3 +29,12 @@ function getHumanChoice() {
     return capitalizeChoice;
 }
 
+
+function playRound(humanChoice, computerChoice) {
+    //Working on :D
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
